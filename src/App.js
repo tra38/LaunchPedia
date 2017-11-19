1459cfb3e2d2735f4f493c9959b0451f818b2f10
tra38 <tra38@nau.edu>
2017-11-19 13:09:59 -0800

    Add datepicker to allow people to view different rockets

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Panel from 'react-bootstrap/lib/Panel';
 import Accordion from 'react-bootstrap/lib/Accordion';
 import Media from 'react-bootstrap/lib/Media';
 import './App.css';
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
-const launchHeader = ({launchDetails}) => (
+const LaunchHeader = ({launchDetails}) => (
   <strong>
     <p>{launchDetails.name}</p>
     <p>Launch Date: {launchDetails.net}</p>
@@ -19,15 +22,15 @@ const RocketImage = ({imageURL, imageSizes}) => {
   return (<img src={newImageURL} thumbnail={true} responsive={true} />)
 }
 
+const LaunchTemplate = ({rocketName, locationName, numberOfAgencies, agencyNames}) => {
+  return (
+      <p>{rocketName} is being prepared for launch at {locationName}. This rocket is controlled by {numberOfAgencies} {agencyNames}.</p>
+  );
+}
+
 class LaunchInfo extends Component {
   constructor() {
     super();
-    this.state = { locationName: "",  agencies: [], imageURL: "", imageSizes: [], rocketName: ""};
-  }
-
-  launchURL(launchId) {
-    var url = `https://launchlibrary.net/1.2/launch/${launchId}`
-    return url
   }
 
   numberOfAgencies(agencies) {
@@ -50,33 +53,18 @@ class LaunchInfo extends Component {
     return array.join(", ")
   }
 
-  componentDidMount() {
-    fetch(this.launchURL(this.props.launchId))
-      .then(launch =>
-        launch.json()
-      )
-      .then(launch => launch.launches[0])
-      .then(data =>
-        this.setState(
-          {
-            rocketName: data.rocket.name,
-            locationName: data.location.name,
-            agencies: data.rocket.agencies,
-            imageURL: data.rocket.imageURL,
-            imageSizes: data.rocket.imageSizes
-          }
-        )
-      );
-  }
-
   render() {
     return (
       <Media className="Launch-info">
         <Media.Left align="top">
-          <RocketImage imageURL={this.state.imageURL} imageSizes = {this.state.imageSizes} />
+          <RocketImage imageURL={this.props.imageURL} imageSizes = {this.props.imageSizes} />
         </Media.Left>
         <Media.Body>
-          {this.state.rocketName} is being prepared for launch at {this.state.locationName}. This rocket is controlled by {this.numberOfAgencies(this.state.agencies)} {this.agencyNames(this.state.agencies)}.
+          <LaunchTemplate
+            rocketName = {this.props.rocketName}
+            locationName = {this.props.locationName}
+            numberOfAgencies = {this.numberOfAgencies(this.props.agencies)}
+            agencyNames = {this.agencyNames(this.props.agencies)} />
         </Media.Body>
       </Media>
     );
@@ -85,32 +73,101 @@ class LaunchInfo extends Component {
 }
 
 const LaunchDiv = ({launchDetails}) => (
-  <Panel header={ launchHeader({launchDetails}) } eventKey={launchDetails.id} className="Launch-div">
-    <LaunchInfo launchId={launchDetails.id}/>
+  <Panel
+    header={ LaunchHeader({launchDetails}) }
+    eventKey={launchDetails.id}
+    className="Launch-div">
+    <LaunchInfo
+      rocketName={launchDetails.rocket.name}
+      locationName={launchDetails.location.name}
+      agencies={launchDetails.rocket.agencies}
+      imageURL={launchDetails.rocket.imageURL}
+      imageSizes={launchDetails.rocket.imageSizes} />
   </Panel>
 );
+
+class LaunchDatePicker extends Component {
+  constructor() {
+    super();
+    this.state = { startDate: null, endDate: null, focusedInput: null }
+  }
+
+  updateDates = (startDate, endDate) => {
+    this.setState({ startDate: startDate, endDate: endDate });
+    this.props.updateParentDate({ startDate: startDate, endDate: endDate });
+  }
+
+  render() {
+    return (
+      <div className="Launch-date-picker">
+        <DateRangePicker
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onDatesChange={({ startDate, endDate }) => this.updateDates(startDate, endDate) }
+          focusedInput={this.state.focusedInput}
+          onFocusChange={focusedInput => this.setState({ focusedInput })}
+          showClearDates={true}
+
+          //Necessary to allow users to pick dates earlier than today:
+          //https://github.com/airbnb/react-dates/issues/239#issuecomment-302574295
+          isOutsideRange={() => false } />
+      </div>
+    );
+  }
+
+}
+
+const LaunchDisplay = ({launches}) => {
+  return (
+    <Accordion>
+        {launches.map(
+          (launchDetails) =>
+            LaunchDiv({ launchDetails })
+        )}
+    </Accordion>
+  );
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { launches: [] };
+    this.state = { launches: [], startDate: null, endDate: null };
   }
 
   componentDidMount() {
-    fetch('https://launchlibrary.net/1.2/launch').
+    this.queryData()
+  }
+
+  launchesURL() {
+    var startDate = this.state.startDate;
+    var endDate = this.state.endDate;
+    if (startDate == null && endDate == null) {
+      return "https://launchlibrary.net/1.3/launch/2017-11-19/2017-12-01"
+    } else if (endDate == null) {
+      var formattedStartDate = startDate.format('YYYY-MM-DD')
+      return `https://launchlibrary.net/1.3/launch/${formattedStartDate}`
+    } else {
+      var formattedStartDate = startDate.format('YYYY-MM-DD')
+      var formattedEndDate = endDate.format('YYYY-MM-DD')
+      return `https://launchlibrary.net/1.3/launch/${formattedStartDate}/${formattedEndDate}`
+    }
+  }
+
+
+  queryData() {
+    var url = this.launchesURL()
+    fetch(url).
       then(result =>
         result.json()
       )
       .then(result => this.setState ({launches: result.launches }));
   }
 
-  renderUsers() {
-    return <Accordion>
-      {this.state.launches.map(
-        (launchDetails) =>
-          LaunchDiv({ launchDetails })
-      )}
-    </Accordion>
+  updateParentDate = (object) => {
+    var startDate = object.startDate
+    var endDate = object.endDate
+    this.setState({ startDate: startDate, endDate: endDate });
+    this.queryData();
   }
 
   render() {
@@ -123,8 +180,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Panel>
+          <LaunchDatePicker updateParentDate={this.updateParentDate}/>
+        </Panel>
         <p className="App-intro">
-          { this.renderUsers() }
+          <LaunchDisplay launches={this.state.launches} />
         </p>
       </div>
     );
