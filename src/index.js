8ceac8b7fb88c86c19e43a4cfb71cef703b38a81
tra38 <tra38@nau.edu>
2017-11-20 12:43:57 -0800

    Move the import index.css statement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import FavoritesStore from './stores/all'
 
 // css files
+import './index.css';
 import './App.css';
 import 'react-dates/lib/css/_datepicker.css';
 
