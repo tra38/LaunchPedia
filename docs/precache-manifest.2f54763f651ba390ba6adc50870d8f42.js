0d5f71e2da6d6254e2a1648f08f6b02e01f3fdda
tra38 <tra38@nau.edu>
2020-06-08 19:34:56 -0500

    Add docs folder

---
+++ docs/precache-manifest.2f54763f651ba390ba6adc50870d8f42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3603cbe4cbe519040d3771a8eaa2d6c2",
+    "url": "/LaunchPedia/index.html"
+  },
+  {
+    "revision": "f161458c7f1116436b2c",
+    "url": "/LaunchPedia/static/css/2.8893375b.chunk.css"
+  },
+  {
+    "revision": "a75040f4fa612115d80b",
+    "url": "/LaunchPedia/static/css/main.e8eb0f68.chunk.css"
+  },
+  {
+    "revision": "f161458c7f1116436b2c",
+    "url": "/LaunchPedia/static/js/2.3aacf11f.chunk.js"
+  },
+  {
+    "revision": "b2462657367731128452d4287487175c",
+    "url": "/LaunchPedia/static/js/2.3aacf11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75040f4fa612115d80b",
+    "url": "/LaunchPedia/static/js/main.d58fbc69.chunk.js"
+  },
+  {
+    "revision": "b8f0c324b42c5d83b78d",
+    "url": "/LaunchPedia/static/js/runtime-main.828780c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LaunchPedia/static/media/logo.5d5d9eef.svg"
+  }
+]);
